ec75a8598e74e7da59d0e86d5171d06e78fd8ba2
zombie110year <zombie110year@outlook.com>
2020-11-19 20:57:07 +0800

    修复（cli）：在没有输入子命令时无响应

--- drcom-cli/src/main.rs
+++ drcom-cli/src/main.rs
@@ -21,49 +21,56 @@ fn main() {
     ]);
     let args = argparser.get_matches();
     if let Some(m) = args.subcommand_matches("run") {
-        let conf_path = m
-            .value_of("config")
-            .and_then(|fp| Some(PathBuf::from(fp)))
-            .or_else(find_config)
-            .expect("找不到可用的配置文件");
-        let conf =
-            load_config(&conf_path).expect(format!("配置文件格式错误: {:?}", conf_path).as_str());
-        env_logger::builder()
-            .filter_level(match conf.behavior.log_level.as_str() {
-                "trace" => LevelFilter::Trace,
-                "debug" => LevelFilter::Debug,
-                "info" => LevelFilter::Info,
-                "warn" => LevelFilter::Warn,
-                "error" => LevelFilter::Error,
-                _ => LevelFilter::Info,
-            })
-            .init();
-
-        info!("使用配置文件 {:?}", conf_path);
-        let mut app = Drcom::new(conf).unwrap();
-        loop {
-            app.login();
-            app.empty_socket_buffer();
-            match app.keep_alive() {
-                Ok(_) => break,
-                Err(DrcomException::KeepAlive1)
-                | Err(DrcomException::KeepAlive2)
-                | Err(DrcomException::KeepAlive3)
-                | Err(DrcomException::KeepAlive4)
-                | Err(DrcomException::StdIOError(_)) => {
-                    error!("KeepAlive Stable Error");
-                    warn!("20 秒后重启");
-                    std::thread::sleep(Duration::new(20, 0));
-                    continue;
-                }
-                Err(e) => {
-                    error!("其他错误 {:?}", e);
-                    panic!("{:?}", e);
-                }
-            }
-        }
+        drcom_run(m)
     } else if let Some(_) = args.subcommand_matches("default-toml") {
         gen_default_config()
+    } else {
+        // 没有输入子命令时认为 run
+        drcom_run(&args)
+    }
+}
+
+fn drcom_run(m: &clap::ArgMatches<'static>) {
+    let conf_path = m
+        .value_of("config")
+        .and_then(|fp| Some(PathBuf::from(fp)))
+        .or_else(find_config)
+        .expect("找不到可用的配置文件");
+    let conf =
+        load_config(&conf_path).expect(format!("配置文件格式错误: {:?}", conf_path).as_str());
+    env_logger::builder()
+        .filter_level(match conf.behavior.log_level.as_str() {
+            "trace" => LevelFilter::Trace,
+            "debug" => LevelFilter::Debug,
+            "info" => LevelFilter::Info,
+            "warn" => LevelFilter::Warn,
+            "error" => LevelFilter::Error,
+            _ => LevelFilter::Info,
+        })
+        .init();
+
+    info!("使用配置文件 {:?}", conf_path);
+    let mut app = Drcom::new(conf).unwrap();
+    loop {
+        app.login();
+        app.empty_socket_buffer();
+        match app.keep_alive() {
+            Ok(_) => break,
+            Err(DrcomException::KeepAlive1)
+            | Err(DrcomException::KeepAlive2)
+            | Err(DrcomException::KeepAlive3)
+            | Err(DrcomException::KeepAlive4)
+            | Err(DrcomException::StdIOError(_)) => {
+                error!("KeepAlive Stable Error");
+                warn!("20 秒后重启");
+                std::thread::sleep(Duration::new(20, 0));
+                continue;
+            }
+            Err(e) => {
+                error!("其他错误 {:?}", e);
+                panic!("{:?}", e);
+            }
+        }
     }
 }
 
